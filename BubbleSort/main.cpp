11db7a552acf6f4e1f40585727e07f8f0a69ad10
adrianh589 <33093678+adrianh589@users.noreply.github.com>
2017-12-01 11:11:32 -0500

    Update main.cpp

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
 using namespace std;
 
-int main()
-{
-
-    return 0;
-}
-
-void BubbleSort(int array*){
-for(int i=0;i<array.length-1;i++)
+void Bubblesort(int A[]){
+    bool swapped;
+    for(int i=0; i<sizeof(A)-1;i++)
     {
-
+        swapped=false;
+        for(int j=0; i<sizeof(A)-1;i++)
+            //compare to adjacent elements
+        if (A[j]>A[j+1])
+        {
+           //swap them
+           int swap=A[j];
+           A[j]=A[j+1];
+           A[j+1]=swap;
+           swapped=true;
+        }
+        //if no number was swapped, the array is sorted now
+        if(not swapped)
+            {
+                break;
+            }
     }
 
+}
+
+/* Function to print an array */
+void printArray(int arr[], int n)
+{
+    for (int i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+
+
+int main(int argc, char **argv)
+{
+   //Aray with numbers
+   int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    Bubblesort(arr);
+    printf("Sorted array : \n");
+    printArray(arr,n);
+
+
+
+    return 0;
 }
